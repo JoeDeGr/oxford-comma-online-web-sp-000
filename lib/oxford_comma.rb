9c8d5c2f37b606871620afb4953d9cbc82100ce5
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-07 22:58:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  array = array.insert(array.length-2, "and " + array.last)
+  array = array.insert(array.length-1, "and " + array.last)
   array.pop
   array.join(", ")
 end
